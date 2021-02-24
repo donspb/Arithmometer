9c009688266732650ec42851b953c36c09b77816
donspb <donspb@gmail.com>
2021-02-24 19:43:48 +0300

    Calc

---
+++ app/build/generated/source/buildConfig/debug/ru/gb/androidone/donspb/arithmometer/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ru.gb.androidone.donspb.arithmometer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ru.gb.androidone.donspb.arithmometer";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
